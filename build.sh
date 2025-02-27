b8dba2e002d90573bf3477f213eb38acb5184163
Matthew Vogel <matt@portlandwebdev.com>
2025-02-27 00:30:34 -0800

    rm build

--- build.sh
+++
@@ -1,6 +0,0 @@
-find . -type f -name '*.jsx' -exec sed -i '' s/config/config.prod/g {} +
-yarn build
-find . -type f -name '*.jsx' -exec sed -i '' s/config.prod/config/g {} +
-rsync -avz --delete ./dist/* 10.0.0.168:/media/john/4tb/home/mvogel/yadacoin/static/wallet
-mv dist/index.html dist/wallet.html
-scp ./dist/wallet.html 10.0.0.168:/media/john/4tb/home/mvogel/yadacoin/templates/wallet.html
